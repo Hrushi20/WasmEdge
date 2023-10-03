cca2f990cdb211575747b6ce53b61ff3ae9187fe
Hrushikesh Rao <hrushikesh20thegreat@gmail.com>
2023-10-03 07:22:10 +0530

    added swresample funcs
    Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

--- plugins/wasmedge_ffmpeg/swresample/module.cpp
+++ plugins/wasmedge_ffmpeg/swresample/module.cpp
@@ -12,6 +12,11 @@ WasmEdgeFFmpegSWResampleModule::
 
     addHostFunc("wasmedge_ffmpeg_swresample_swresample_getVersion",std::make_unique<SWResampleVersion>(Env));
     addHostFunc("wasmedge_ffmpeg_swresample_swr_get_delay",std::make_unique<SWRGetDelay>(Env));
+    addHostFunc("wasmedge_ffmpeg_swresample_swr_init",std::make_unique<SWRInit>(Env));
+    addHostFunc("wasmedge_ffmpeg_swresample_swr_alloc_set_opts",std::make_unique<SWRAllocSetOpts>(Env));
+    addHostFunc("wasmedge_ffmpeg_swresample_av_opt_set_dict",std::make_unique<AVOptSetDict>(Env));
+    addHostFunc("wasmedge_ffmpeg_swresample_swr_convert_frame",std::make_unique<SWRConvertFrame>(Env));
+    addHostFunc("wasmedge_ffmpeg_swresample_swr_free",std::make_unique<SWRFree>(Env));
 }
 
 
