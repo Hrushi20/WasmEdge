6712f1024ae944d6be432b0811a72ec29fefcdd8
Hrushikesh Rao <hrushikesh20thegreat@gmail.com>
2023-10-03 07:22:10 +0530

    added swresample funcs
    Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

--- plugins/wasmedge_ffmpeg/swresample/swresample_func.h
+++ plugins/wasmedge_ffmpeg/swresample/swresample_func.h
@@ -1,8 +1,9 @@
 #pragma once
 
-#include "runtime/instance/module.h"
+#include "runtime/callingframe.h"
 #include "swresample_base.h"
 #include "ffmpeg_env.h"
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 
 namespace WasmEdge {
 namespace Host {
@@ -21,6 +22,42 @@ public:
   SWRGetDelay(std::shared_ptr<WasmEdgeFFmpegEnv> HostEnv)
       : WasmEdgeFFmpegSWResample(HostEnv) {}
   Expect<int64_t> body(const Runtime::CallingFrame &Frame,uint32_t SWRContextId,int64_t Base);
+};
+
+class SWRInit: public WasmEdgeFFmpegSWResample<SWRInit> {
+public:
+  SWRInit(std::shared_ptr<WasmEdgeFFmpegEnv> HostEnv)
+      : WasmEdgeFFmpegSWResample(HostEnv) {}
+  Expect<int32_t> body(const Runtime::CallingFrame &Frame,uint32_t SWRContextId);
+};
+
+class SWRAllocSetOpts : public WasmEdgeFFmpegSWResample<SWRAllocSetOpts> {
+public:
+  SWRAllocSetOpts(std::shared_ptr<WasmEdgeFFmpegEnv> HostEnv)
+      : WasmEdgeFFmpegSWResample(HostEnv) {}
+  Expect<int32_t> body(const Runtime::CallingFrame &Frame,uint32_t SwrCtxPtr,uint32_t SWRContextId,int64_t OutChLayout,uint32_t OutSampleFmtId,int32_t OutSampleRate,int64_t InChLayout, uint32_t InSampleFmtId,int32_t InSampleRate,int32_t LogOffset);
+};
+
+class AVOptSetDict : public WasmEdgeFFmpegSWResample<AVOptSetDict> {
+public:
+  AVOptSetDict(std::shared_ptr<WasmEdgeFFmpegEnv> HostEnv)
+      : WasmEdgeFFmpegSWResample(HostEnv) {}
+  Expect<int32_t> body(const Runtime::CallingFrame &Frame,uint32_t SWRContextId,uint32_t DictId);
+};
+
+class SWRConvertFrame: public WasmEdgeFFmpegSWResample<SWRConvertFrame> {
+public:
+  SWRConvertFrame(std::shared_ptr<WasmEdgeFFmpegEnv> HostEnv)
+      : WasmEdgeFFmpegSWResample(HostEnv) {}
+  Expect<int32_t> body(const Runtime::CallingFrame &Frame,uint32_t SWRContextId,uint32_t FrameOutputId, uint32_t FrameInputId);
+};
+
+
+class SWRFree: public WasmEdgeFFmpegSWResample<SWRFree> {
+public:
+  SWRFree(std::shared_ptr<WasmEdgeFFmpegEnv> HostEnv)
+      : WasmEdgeFFmpegSWResample(HostEnv) {}
+  Expect<int32_t> body(const Runtime::CallingFrame &Frame,uint32_t SWRContextId);
 };
 
 } // namespace SWResample
