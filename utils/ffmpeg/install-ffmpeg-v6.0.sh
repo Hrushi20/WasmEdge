f42638069a9fb0061592fea0e6127ae81b093e68
Hrushi20 <hrushikesh20thegreat@gmail.com>
2023-11-23 21:27:33 +0530

    update install-ffmpeg script
    Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>
    
    attempt to fix PKG_CONFIG_PATH
    
    Signed-off-by: Hrushi20 <hrushikesh20thegreat@gmail.com>

--- utils/ffmpeg/install-ffmpeg-v6.0.sh
+++ utils/ffmpeg/install-ffmpeg-v6.0.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
+rm -rf FFmpeg-n6.0 ffmpeg.zip
+echo $(pwd)
 
 curl -sL https://github.com/FFmpeg/FFmpeg/archive/refs/tags/n6.0.zip -o ffmpeg.zip
 
 unzip ffmpeg.zip
 
 cd FFmpeg-n6.0
-./configure --prefix=/usr/local --enable-gpl --enable-nonfree --enable-shared --disable-static
-make install
+mkdir -p output
+./configure --prefix=$(pwd)/output --enable-gpl --enable-nonfree --enable-shared --disable-static
+
+cd ../..
+echo $(pwd)
+
+#Setting PKG_CONFIG_PATH to be discoverd
+PKG_CONFIG_PATH=$(pwd)/WasmEdge/FFmpeg-n6.0/output/lib/pkgconfig cmake -Bbuild_pr2885 -GNinja -DCMAKE_BUILD_TYPE=Release -DWASMEDGE_BUILD_TESTS=ON  -DWASMEDGE_PLUGIN_FFMPEG=ON .
